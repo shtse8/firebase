58d8275db06f9dd8c67fa309333885e36853a006
shtse8 <shtse8@gmail.com>
2024-08-11 07:06:38 +0800

    feat(JsonDiffer): Implement JSON comparison with array value treatment
    - Add JsonDiffer class with diff method for comparing JSON objects
    - Implement deep comparison of nested objects and arrays
    - Add option to treat arrays as values with detailed change detection
    - Create sealed DiffChange class hierarchy for type-safe change representation
    - Optimize array comparison to detect unions and removals
    - Implement moved item detection for arrays
    - Use DeepCollectionEquality for accurate value comparisons
    
    This commit introduces a robust JSON diffing tool that can handle complex
    nested structures and provides detailed change information. The new
    treatArrayAsValue option allows for flexible array comparison strategies.

--- lib/firestore/odm/firestore_document.dart
+++ lib/firestore/odm/firestore_document.dart
@@ -1,11 +1,191 @@
 import 'dart:async';
 import 'dart:developer';
-import 'package:json_diff/json_diff.dart';
 
 import 'firestore_converter.dart';
 import 'firestore_sdk_interface.dart';
 import 'firestore_types.dart';
 import 'package:collection/collection.dart';
+
+sealed class DiffChange {
+  const DiffChange();
+}
+
+class AddedChange extends DiffChange {
+  final dynamic value;
+  const AddedChange(this.value);
+}
+
+class RemovedChange extends DiffChange {
+  final dynamic value;
+  const RemovedChange(this.value);
+}
+
+class ModifiedChange extends DiffChange {
+  final dynamic oldValue;
+  final dynamic newValue;
+  const ModifiedChange(this.oldValue, this.newValue);
+}
+
+class MovedChange extends DiffChange {
+  final int oldIndex;
+  final int newIndex;
+  const MovedChange(this.oldIndex, this.newIndex);
+}
+
+class NestedChange extends DiffChange {
+  final DiffNode node;
+  const NestedChange(this.node);
+}
+
+class ArrayUnionChange extends DiffChange {
+  final List union;
+  const ArrayUnionChange(this.union);
+}
+
+class ArrayRemoveChange extends DiffChange {
+  final List remove;
+  const ArrayRemoveChange(this.remove);
+}
+
+class DiffNode {
+  DiffNode(this.path);
+
+  final List<Object> path;
+  final Map<Object, DiffChange> changes = {};
+
+  void addChange(Object key, DiffChange change) {
+    changes[key] = change;
+  }
+
+  bool get hasChanges => changes.isNotEmpty;
+
+  @override
+  String toString() => 'DiffNode(path: $path, changes: $changes)';
+}
+
+class JsonDiffer {
+  static final _deepEquality = DeepCollectionEquality();
+
+  static DiffNode diff(Map leftJson, Map rightJson,
+      {bool treatArrayAsValue = false}) {
+    return _compareMaps(leftJson, rightJson, [], treatArrayAsValue);
+  }
+
+  static DiffNode _compareMaps(
+      Map left, Map right, List<Object> path, bool treatArrayAsValue) {
+    final diffNode = DiffNode(path);
+
+    for (final key in left.keys) {
+      if (!right.containsKey(key)) {
+        diffNode.addChange(key, RemovedChange(left[key]));
+      } else {
+        final leftValue = left[key];
+        final rightValue = right[key];
+        if (leftValue is Map && rightValue is Map) {
+          final childDiff = _compareMaps(
+              leftValue, rightValue, [...path, key], treatArrayAsValue);
+          if (childDiff.hasChanges) {
+            diffNode.addChange(key, NestedChange(childDiff));
+          }
+        } else if (leftValue is List && rightValue is List) {
+          if (treatArrayAsValue) {
+            final arrayChange = _compareArraysAsValues(leftValue, rightValue);
+            if (arrayChange != null) {
+              diffNode.addChange(key, arrayChange);
+            }
+          } else {
+            final childDiff =
+                _compareLists(leftValue, rightValue, [...path, key]);
+            if (childDiff.hasChanges) {
+              diffNode.addChange(key, NestedChange(childDiff));
+            }
+          }
+        } else if (!_deepEquality.equals(leftValue, rightValue)) {
+          diffNode.addChange(key, ModifiedChange(leftValue, rightValue));
+        }
+      }
+    }
+
+    for (final key in right.keys) {
+      if (!left.containsKey(key)) {
+        diffNode.addChange(key, AddedChange(right[key]));
+      }
+    }
+
+    return diffNode;
+  }
+
+  static DiffNode _compareLists(List left, List right, List<Object> path) {
+    final diffNode = DiffNode(path);
+    final len = left.length > right.length ? left.length : right.length;
+
+    for (var i = 0; i < len; i++) {
+      if (i >= left.length) {
+        diffNode.addChange(i, AddedChange(right[i]));
+      } else if (i >= right.length) {
+        diffNode.addChange(i, RemovedChange(left[i]));
+      } else {
+        final leftValue = left[i];
+        final rightValue = right[i];
+        if (leftValue is Map && rightValue is Map) {
+          final childDiff =
+              _compareMaps(leftValue, rightValue, [...path, i], false);
+          if (childDiff.hasChanges) {
+            diffNode.addChange(i, NestedChange(childDiff));
+          }
+        } else if (leftValue is List && rightValue is List) {
+          final childDiff = _compareLists(leftValue, rightValue, [...path, i]);
+          if (childDiff.hasChanges) {
+            diffNode.addChange(i, NestedChange(childDiff));
+          }
+        } else if (!_deepEquality.equals(leftValue, rightValue)) {
+          diffNode.addChange(i, ModifiedChange(leftValue, rightValue));
+        }
+      }
+    }
+
+    _detectMovedItems(left, right, diffNode);
+
+    return diffNode;
+  }
+
+  static DiffChange? _compareArraysAsValues(List left, List right) {
+    if (_deepEquality.equals(left, right)) {
+      return null;
+    }
+
+    if (right.length > left.length &&
+        left.isNotEmpty &&
+        _deepEquality.equals(left, right.sublist(0, left.length))) {
+      return ArrayUnionChange(right.sublist(left.length));
+    }
+
+    if (left.length > right.length &&
+        right.isNotEmpty &&
+        _deepEquality.equals(right, left.sublist(0, right.length))) {
+      return ArrayRemoveChange(left.sublist(right.length));
+    }
+
+    return ModifiedChange(left, right);
+  }
+
+  static void _detectMovedItems(List left, List right, DiffNode diffNode) {
+    final leftIndexMap = <dynamic, int>{};
+    for (var i = 0; i < left.length; i++) {
+      leftIndexMap[left[i]] = i;
+    }
+
+    for (var newIndex = 0; newIndex < right.length; newIndex++) {
+      final item = right[newIndex];
+      if (leftIndexMap.containsKey(item)) {
+        final oldIndex = leftIndexMap[item]!;
+        if (oldIndex != newIndex) {
+          diffNode.addChange(oldIndex, MovedChange(oldIndex, newIndex));
+        }
+      }
+    }
+  }
+}
 
 abstract class FirestoreDocument<T> {
   final FirestoreSDK sdk;
@@ -44,12 +224,11 @@ abstract class FirestoreDocument<T> {
         : <String, dynamic>{};
     final updatedMap = converter.toJson(updatedData);
 
-    log('Updating currentMap: $currentMap');
-    log('Updating updatedMap: $updatedMap');
     final updates =
         generateUpdates(currentMap, updatedMap, noTransform: noTransform);
 
     if (updates.isNotEmpty) {
+      // log('Updating document $path:\ncurrentMap: $currentMap\nupdatedMap: $updatedMap\nupdates: $updates');
       log('Updating document $path with updates: $updates');
       await sdk.updateDocument(path, updates);
     }
@@ -76,63 +255,51 @@ abstract class FirestoreDocument<T> {
     Map<String, dynamic> newData, {
     bool noTransform = false,
   }) {
-    final differ = JsonDiffer.fromJson(oldData, newData);
     final updates = <String, dynamic>{};
 
     void processNode(DiffNode node) {
       String buildFieldPath(Object key) =>
           [...node.path, key.toString()].join('.');
 
-      // Handle added fields
-      node.added.forEach((key, value) {
-        updates[buildFieldPath(key)] = value;
-      });
-
-      // Handle removed fields
-      node.removed.forEach((key, _) {
-        updates[buildFieldPath(key)] = FieldValue.delete();
-      });
-
-      // Handle changed fields
-      node.changed.forEach((key, value) {
-        final [oldValue, newValue] = value;
+      node.changes.forEach((key, change) {
         final fieldPath = buildFieldPath(key);
-
-        if (noTransform) {
-          updates[fieldPath] = newValue;
-        } else if (oldValue is List && newValue is List) {
-          _handleListChanges(updates, fieldPath, oldValue, newValue);
-        } else if (oldValue is num && newValue is num) {
-          updates[fieldPath] = FieldValue.increment(newValue - oldValue);
-        } else {
-          updates[fieldPath] = newValue;
+        switch (change) {
+          case AddedChange addedChange:
+            updates[fieldPath] = addedChange.value;
+            break;
+          case RemovedChange _:
+            updates[fieldPath] = FieldValue.delete();
+            break;
+          case ModifiedChange modifiedChange:
+            if (noTransform) {
+              updates[fieldPath] = modifiedChange.newValue;
+            } else if (modifiedChange.oldValue is num &&
+                modifiedChange.newValue is num) {
+              updates[fieldPath] = FieldValue.increment(
+                  modifiedChange.newValue - modifiedChange.oldValue);
+            } else {
+              updates[fieldPath] = modifiedChange.newValue;
+            }
+            break;
+          case MovedChange _:
+            // Do nothing for moved items
+            break;
+          case NestedChange change:
+            processNode(change.node);
+            break;
+          case ArrayUnionChange arrayUnionChange:
+            updates[fieldPath] = FieldValue.arrayUnion(arrayUnionChange.union);
+            break;
+          case ArrayRemoveChange arrayRemoveChange:
+            updates[fieldPath] =
+                FieldValue.arrayRemove(arrayRemoveChange.remove);
+            break;
         }
       });
-
-      // Process nested nodes
-      node.node.forEach((key, value) {
-        processNode(value);
-      });
     }
 
-    processNode(differ.diff());
+    final node = JsonDiffer.diff(oldData, newData, treatArrayAsValue: true);
+    processNode(node);
     return updates;
-  }
-
-  static void _handleListChanges(Map<String, dynamic> updates, String fieldPath,
-      List oldValue, List newValue) {
-    const deepEq = DeepCollectionEquality();
-    if (newValue.length > oldValue.length &&
-        deepEq.equals(newValue.sublist(0, oldValue.length), oldValue)) {
-      updates[fieldPath] =
-          FieldValue.arrayUnion(newValue.sublist(oldValue.length));
-    } else if (oldValue.length > newValue.length &&
-        newValue.every((element) => oldValue.contains(element))) {
-      final removedElements =
-          oldValue.where((element) => !newValue.contains(element)).toList();
-      updates[fieldPath] = FieldValue.arrayRemove(removedElements);
-    } else if (!deepEq.equals(oldValue, newValue)) {
-      updates[fieldPath] = newValue;
-    }
   }
 }
